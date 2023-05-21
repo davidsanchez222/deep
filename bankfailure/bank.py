16bc543ec62f649efbaf86a8a9b3211424b80ed5
David <david@david.david>
2023-05-20 23:41:31 -0400

    hello

--- bankfailure/bank.py
+++ bankfailure/bank.py
@@ -11,5 +11,14 @@ from lazypredict.Supervised import LazyRegressor
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 #%%
+df = pd.read_csv(r"C:\Users\David\PycharmProjects\deep\bankfailure\bankdata.csv", parse_dates=['FAILDATE'])
 
+df.shape[0]
+# 4104
+
+df["STATE"] = df.apply(lambda row: row["CITYST"][-2:], axis=1)
+df["STATE"].value_counts()[:3]
+# TX    910
+# CA    263
+# IL    227
 
